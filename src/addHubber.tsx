8a195ae4045a812b3954faac4c5848624c92f1b3
Jonathan Clem <jclem@github.com>
2021-12-09 13:41:59 -0500

    Add additional context option to Add Hubber

--- src/addHubber.tsx
+++ src/addHubber.tsx
@@ -1,6 +1,8 @@
 import {
   ActionPanel,
   Form,
+  FormTagPicker,
+  FormTagPickerItem,
   FormTextField,
   popToRoot,
   preferences,
@@ -8,13 +10,41 @@ import {
   SubmitFormAction,
   ToastStyle
 } from '@raycast/api'
-import {notion} from './lib/notion'
+import {useEffect, useState} from 'react'
+import {notion, peopleDatabaseId} from './lib/notion'
 
 export default () => {
+  const [additionalContext, setAdditionalContext] = useState<string[]>([])
+
+  useEffect(() => {
+    const fetchContexts = async () => {
+      const resp = await notion.databases.retrieve({
+        database_id: peopleDatabaseId
+      })
+      const contextProp = resp.properties.Context
+
+      if (contextProp?.type === 'multi_select') {
+        setAdditionalContext(
+          contextProp.multi_select.options
+            .filter(o => o.name !== 'GitHub')
+            .map(o => o.name)
+        )
+      }
+    }
+
+    fetchContexts()
+  }, [])
+
   return (
     <Form actions={<Actions />}>
       <FormTextField id="name" title="Name" />
       <FormTextField id="handle" title="Handle" />
+
+      <FormTagPicker id="context" title="Context">
+        {additionalContext.map(ctx => (
+          <FormTagPickerItem key={ctx} title={ctx} value={ctx} />
+        ))}
+      </FormTagPicker>
     </Form>
   )
 }
@@ -22,7 +52,15 @@ export default () => {
 const Actions = () => {
   const dbID = String(preferences.peopleDatabaseID.value)
 
-  const onSubmit = async ({name, handle}: {name: string; handle: string}) => {
+  const onSubmit = async ({
+    name,
+    handle,
+    context
+  }: {
+    name: string
+    handle: string
+    context: string[]
+  }) => {
     const avatarURL = `https://github.com/${handle}.png`
 
     try {
@@ -33,7 +71,9 @@ const Actions = () => {
           'GitHub Handle': {
             rich_text: [{type: 'text', text: {content: handle}}]
           },
-          Context: {multi_select: [{name: 'GitHub'}]},
+          Context: {
+            multi_select: [{name: 'GitHub'}, ...context.map(name => ({name}))]
+          },
           Photo: {files: [{name: avatarURL, external: {url: avatarURL}}]}
         }
       })
